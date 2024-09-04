aeceb76834bb5036615fae53224a048e5a32466e
Caleb Stimpson <caleb.stimpson@gmail.com>
2024-09-04 10:36:11 -0600

    Resize canvas to full screen

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,18 @@
 import './style.css'
+
+const canvas = document.createElement('canvas');
+canvas.className = 'layer layer/base';
+document.body.prepend(canvas);
+
+function resizeCanvas() {
+  const { width, height } = canvas.getBoundingClientRect();
+  canvas.width = Math.floor(width * devicePixelRatio);
+  canvas.height = Math.floor(height * devicePixelRatio);
+  paint(canvas.getContext('2d')!);
+}
+resizeCanvas();
+document.addEventListener('resize', resizeCanvas);
+
+function paint(ctx: CanvasRenderingContext2D) {
+  
+}
